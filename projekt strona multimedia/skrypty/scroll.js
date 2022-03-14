e7e636e2ec33712af2c36793b6119a9f427db829
staaaa <90243711+staaaa@users.noreply.github.com>
2022-03-14 09:24:31 +0100

    Add files via upload

---
+++ projekt strona multimedia/skrypty/scroll.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $(".btn1").click(function() {
+        $(".c1").css("display","block");
+        $(".c1").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c1").offset().top - 12.5},
+            'slow');
+        $(".c1").animate({
+            height:"800px",
+        },1250);
+    });
+    $(".btn2").click(function() {
+        $(".c2").css("display","block");
+        $(".c2").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c2").offset().top - 12.5},
+            'slow');
+        $(".c2").animate({
+            height:"800px",
+        },1250);
+    });
+    $(".btn3").click(function() {
+        $(".c3").css("display","block");
+        $(".c3").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c3").offset().top - 12.5},
+            'slow');
+        $(".c3").animate({
+            height:"800px",
+        },1250);
+    });
+    $(".btn4").click(function() {
+        $(".c4").css("display","block");
+        $(".c4").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c4").offset().top - 12.5},
+            'slow');
+        $(".c4").animate({
+            height:"800px",
+        },1250);
+    });
+    $(".btn5").click(function() {
+        $(".c5").css("display","block");
+        $(".c5").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c5").offset().top - 12.5},
+            'slow');
+        $(".c5").animate({
+            height:"800px",
+        },1250);
+    });
+    $(".btn6").click(function() {
+        $(".c6").css("display","block");
+        $(".c6").css("visibility","visible");
+        $('html,body').animate({
+            scrollTop: $(".c6").offset().top - 12.5},
+            'slow');
+        $(".c6").animate({
+            height:"800px",
+        },1250);
+    });
+});
