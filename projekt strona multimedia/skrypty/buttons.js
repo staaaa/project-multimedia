de7d243521b41ec54a48a2afd0aa6830e7d6d507
Stanisław Bożych <stachubozych@gmail.com>
2022-03-27 22:32:55 +0200

    yes

--- projekt strona multimedia/skrypty/buttons.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".btn1").mouseover(function() {
-        $(".btn1").animate({
-            rotate(360deg);
-        },500);
-    });
-});
--- projekt strona multimedia/skrypty/scroll.js
+++ projekt strona multimedia/skrypty/scroll.js
@@ -2,91 +2,61 @@ $(document).ready(function(){
     $(".btn1").click(function() {
         $(".c1").css("display","block");
         $(".c1").css("visibility","visible");
-        $('html,body').animate({
-            scrollTop: $(".c1").offset().top - 12.5},
-            'slow');
         $(".c1").animate({
-            height:"800px",
-        },1250);
+            height:"100%",
+        },0);
+        $('html,body').animate({
+            scrollTop: $(".c1").offset().top},
+            'slow'); 
     });
     $(".btn2").click(function() {
         $(".c2").css("display","block");
         $(".c2").css("visibility","visible");
+        $(".c2").animate({
+            height:"100%",
+        },0);
         $('html,body').animate({
             scrollTop: $(".c2").offset().top - 12.5},
             'slow');
-        $(".c2").animate({
-            height:"800px",
-        },1250);
     });
     $(".btn3").click(function() {
         $(".c3").css("display","block");
         $(".c3").css("visibility","visible");
+        $(".c3").animate({
+            height:"100%",
+        },0);
         $('html,body').animate({
             scrollTop: $(".c3").offset().top - 12.5},
             'slow');
-        $(".c3").animate({
-            height:"800px",
-        },1250);
     });
     $(".btn4").click(function() {
         $(".c4").css("display","block");
         $(".c4").css("visibility","visible");
+        $(".c4").animate({
+            height:"100%",
+        },0);
         $('html,body').animate({
             scrollTop: $(".c4").offset().top - 12.5},
             'slow');
-        $(".c4").animate({
-            height:"800px",
-        },1250);
     });
     $(".btn5").click(function() {
         $(".c5").css("display","block");
         $(".c5").css("visibility","visible");
+        $(".c5").animate({
+            height:"100%",
+        },0);
         $('html,body').animate({
             scrollTop: $(".c5").offset().top - 12.5},
             'slow');
-        $(".c5").animate({
-            height:"800px",
-        },1250);
     });
     $(".btn6").click(function() {
         $(".c6").css("display","block");
         $(".c6").css("visibility","visible");
+        $(".c6").animate({
+            height:"100%",
+        },0);
         $('html,body').animate({
             scrollTop: $(".c6").offset().top - 12.5},
             'slow');
-        $(".c6").animate({
-            height:"800px",
-        },1250);
-    });
-    $(".btn7").click(function() {
-        $(".c7").css("display","block");
-        $(".c7").css("visibility","visible");
-        $('html,body').animate({
-            scrollTop: $(".c7").offset().top - 12.5},
-            'slow');
-        $(".c7").animate({
-            height:"800px",
-        },1250);
-    });
-    $(".btn8").click(function() {
-        $(".c8").css("display","block");
-        $(".c8").css("visibility","visible");
-        $('html,body').animate({
-            scrollTop: $(".c8").offset().top - 12.5},
-            'slow');
-        $(".c8").animate({
-            height:"800px",
-        },1250);
-    });
-    $(".btn9").click(function() {
-        $(".c9").css("display","block");
-        $(".c9").css("visibility","visible");
-        $('html,body').animate({
-            scrollTop: $(".c9").offset().top - 12.5},
-            'slow');
-        $(".c9").animate({
-            height:"800px",
-        },1250);
     });
 });
